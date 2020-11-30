23ec123c86c2c764e1acfb21918d1a1c5cf6cb8e
Valeriy Khoma <bergamolt22@gmail.com>
2020-11-30 17:52:22 +0200

    update files

--- js/app.js
+++ js/app.js
@@ -1,42 +1,49 @@
 'use strict';
 
-const field = document.querySelector('.field');
 const tapContainer = document.querySelector('.tap');
 const pauseBtn = document.querySelector('#pause');
+const menuBtn = document.querySelector('#menu');
 const continueBtn = document.querySelector('#continue');
+const continueGameBtn = document.querySelector('#continueGame');
 const beep = new Audio('./audio/beep.wav');
 
-const sizeTile = 100;
-let tapCounter = 1;
-let pause = false;
+//variables
+let tapCounter = 0;
+let pauseCheck = false;
+let checkSound = true;
 let timeMinute = 0;
 let timeSecond = 0;
-
-let emptyPos = {
-    x: 0,
-    y: 0
-};
+let timer;
+let emptyPos = {x: 0, y: 0};
 
 const numbers = [...Array(15).keys()]
     .sort(() => Math.random() - 0.5);
 
 const time = () => {
-    console.log('done')
     const timeBlock = document.querySelector('.time');
-    let minute = timeMinute;
-    let second = timeSecond;
 
     const timer = setInterval(() => {
+        let minute = timeMinute < 10 ? '0' + timeMinute : timeMinute;
+        let second = timeSecond < 10 ? '0' + ++timeSecond : ++timeSecond;
+
         if (timeSecond > 59) {
-            minute++;
-            second = 0;
+            ++timeMinute;
+            timeSecond = 0;
         }
-        timeBlock.textContent = `${minute < 10 ? '0' + minute : minute}:${second < 10 ? '0' + second++ : second++}`;
-        timeMinute = minute;
-        timeSecond = second;
+
+        timeBlock.textContent = `${minute}:${second}`;
     }, 1000);
 
     return timer;
+};
+
+const sound = () => {
+    const btnSound = document.querySelector('#sound');
+
+    btnSound.addEventListener('click', () => {
+        checkSound = !checkSound;
+        btnSound.textContent = !checkSound ? 'Включить звук' : 'Выключить звук';
+    });
 };
 
 const move = (tile) => {
@@ -47,33 +54,39 @@ const move = (tile) => {
     const diffY = Math.abs(savePosY - emptyPos.y);
 
 
-    if (diffY + diffX > 100) {
-        return;
-    } else {
-        tile.style.top = emptyPos.y + 'px';
-        tile.style.left = emptyPos.x + 'px';
+    if (diffY + diffX > 100) return;
 
-        tile.top = emptyPos.y;
-        tile.left = emptyPos.x;
+    tile.style.top = emptyPos.y + 'px';
+    tile.style.left = emptyPos.x + 'px';
+    tile.top = emptyPos.y;
+    tile.left = emptyPos.x;
+    emptyPos.y = savePosY;
+    emptyPos.x = savePosX;
 
-        emptyPos.y = savePosY;
-        emptyPos.x = savePosX;
-        tapContainer.textContent = tapCounter++;
-        beep.play();
-    }
+    tapContainer.textContent = ++tapCounter;
+
+    if (checkSound) beep.play();
+
+    const tiles = [...document.querySelectorAll('.tile')];
+    saveGame(tiles);
+    const isFinishGame = tiles.every(item => {
+        return (item.top * 4 + item.left + 100) / 100 == item.dataset.tileNumber;
+    });
+
+    console.log(isFinishGame);
 };
 
 const render = (num) => {
+    const field = document.querySelector('.field');
+    let sizeTile = field.clientWidth / 4;
     const color = randomColor({
         format: 'rgba',
         alpha: 0.9
     });
-
     const tile = document.createElement('div');
     tile.classList.add('tile');
-    tile.innerHTML = `
-            <div class="tile-item" style="background-color:${color};">${numbers[num - 1] + 1}</div>
-        `;
+    tile.setAttribute('data-tile-number', numbers[num - 1] + 1);
+    tile.innerHTML = `<div class="tile-item" style="background-color:${color};">${numbers[num - 1] + 1}</div>`;
     field.append(tile);
 
     const posX = num % 4;
@@ -88,36 +101,117 @@ const render = (num) => {
     return tile;
 };
 
+//save game
+const saveGame = (elem) => {
+    const obj = {
+        minute: timeMinute,
+        second: timeSecond,
+        taps: tapCounter,
+        elem: elem,    
+    }
+    const json = JSON.stringify(obj);
+    localStorage.setItem('save', json);
+};
+//
+
+const renderContinueGame = (num) => {
+    const obj = JSON.parse(localStorage.getItem('save'));
+        const pos = obj.elem;
+        const color = randomColor({
+            format: 'rgba',
+            alpha: 0.9
+        });
+        const tile = document.createElement('div');
+        tile.classList.add('tile');
+        tile.setAttribute('data-tile-number', num + 1);
+        tile.innerHTML = `<div class="tile-item" style="background-color:${color};">${num + 1}</div>`;
+        field.append(tile);
+
+        tile.style.top = `${pos[num].top}px`;
+        tile.style.left = `${pos[num].left}px`;
+
+        tile.top = pos[num].top;
+        tile.left = pos[num].left;
+
+        return tile;
+}
+
+const pause = () => {
+    const popUp = document.querySelector('.popup');
+    const popUpPauseScreen = document.querySelector('.popup-pause');
+
+    pauseBtn.addEventListener('click', () => {
+        if (!pauseCheck) {
+            popUp.style.height = '100%';
+            popUpPauseScreen.style.display = 'flex';
+            pauseCheck = true;
+            clearInterval(timer);
+        }
+    });
+    
+    continueBtn.addEventListener('click', () => {
+        if (pauseCheck) {
+            popUp.style.height = '0%';
+            popUpPauseScreen.style.display = 'none';
+            timer = time();
+            pauseCheck = false;
+        }
+    });
+};
+
+//start game
+const start = () => {
+    const start = document.querySelector('#startGame');
+};
+//
+
 window.onload = () => {
     document.querySelector('#startGame').addEventListener('click', () => {
         document.querySelector('.start-game').style.display = 'none';
-        document.querySelector('.screen-game').style.display = 'block';
+        document.querySelector('.screen-game').style.display = 'flex';
 
         for (let i = 1; i <= 15; i++) {
             const tile = render(i);
             tile.addEventListener('click', () => {
-                move(tile)
+                move(tile);
             });
         }
-        
-        let timer = time();
 
-        pauseBtn.addEventListener('click', () => {
-            if (!pause) {
+        timer = time();
+        pause();
+        sound();
+
+        //Menu
+        menuBtn.addEventListener('click', () => {
+            if (!pauseCheck) {
                 document.querySelector('.popup').style.height = '100%';
-                document.querySelector('.popup-pause').style.display = 'block';
-                pause = true;
+                document.querySelector('.popup-menu').style.display = 'flex';
+                pauseCheck = true;
                 clearInterval(timer);
+                saveGame();
             }
         });
         
-        continueBtn.addEventListener('click', () => {
-            if (pause) {
+        continueGameBtn.addEventListener('click', () => {
+            if (pauseCheck) {
                 document.querySelector('.popup').style.height = '0%';
-                document.querySelector('.popup-pause').style.display = 'none';
+                document.querySelector('.popup-menu').style.display = 'none';
                 timer = time();
-                pause = false;
+                pauseCheck = false;
             }
         });
-    })
-}
+    });
+
+    const loadSaveBtn = document.querySelector('#loadSaveBtn');
+        loadSaveBtn.addEventListener('click', () => {
+            document.querySelector('.start-game').style.display = 'none';
+        document.querySelector('.screen-game').style.display = 'flex';
+            for (let i = 0; i < 15; i++) {
+                renderContinueGame(i);
+                // const tile = render(i);
+                // tile.addEventListener('click', () => {
+                //     move(tile);
+                // });
+            }
+        });
+};
